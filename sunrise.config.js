c70cabbaad9bda62f9eb37d537d3e09b9d56059d
RaviChandra-deloitte <ravicp@deloitte.com>
2023-04-06 19:17:33 +0530

    Whirlpool initial setup

--- sunrise.config.js
+++ sunrise.config.js
@@ -1,43 +1,15 @@
-const getEnv = (env) => {
-  return typeof global?.Cypress?.env === 'function'
-    ? global.Cypress.env(env)
-    : process.env[env];
-};
-let localConfig = {};
-if (getEnv('VUE_APP_LOCAL_SUNRISE_CONFIG')) {
-  localConfig = require(process.env
-    .VUE_APP_LOCAL_SUNRISE_CONFIG).default;
-}
-const config = {
+export default {
   ct: {
     auth: {
-      host:
-        getEnv('VUE_APP_CT_AUTH_HOST') ||
-        'https://auth.europe-west1.gcp.commercetools.com',
-      projectKey:
-        getEnv('VUE_APP_CT_PROJECT_KEY') || 'sunrise-spa',
+      host: process.env.VUE_APP_CT_AUTH_HOST,
+      projectKey: process.env.VUE_APP_CT_PROJECT_KEY,
       credentials: {
-        clientId:
-          getEnv('VUE_APP_CT_CLIENT_ID') ||
-          '1mnlpBq-fHCCkAzmSXxNBB37',
-        clientSecret:
-          getEnv('VUE_APP_CT_CLIENT_SECRET') ||
-          'WS9hXm6dKyqyuLOHciL6jkbCbFHrDSOL',
+        clientId: process.env.VUE_APP_CT_CLIENT_ID,
+        clientSecret: process.env.VUE_APP_CT_CLIENT_SECRET,
       },
-      scope:
-        getEnv('VUE_APP_CT_SCOPE') ||
-        'manage_my_orders:sunrise-spa ' +
-          'manage_my_profile:sunrise-spa ' +
-          'manage_my_payments:sunrise-spa ' +
-          'view_published_products:sunrise-spa ' +
-          'view_categories:sunrise-spa ' +
-          'manage_my_shopping_lists:sunrise-spa ' +
-          'manage_orders:sunrise-spa ' +
-          'create_anonymous_token:sunrise-spa',
+      scope: [process.env.VUE_APP_CT_SCOPE],
     },
-    api:
-      getEnv('VUE_APP_CT_API_HOST') ||
-      'https://api.europe-west1.gcp.commercetools.com',
+    api: process.env.VUE_APP_CT_API_HOST,
   },
   languages: {
     en: 'English',
@@ -83,37 +55,6 @@ const config = {
   categories: {
     salesExternalId: '6',
   },
-  facetSearches: [
-    {
-      name: 'size',
-      type: 'text',
-      label: {
-        it: 'Size',
-        de: 'Größe',
-        en: 'Size',
-      },
-    },
-    {
-      name: 'color',
-      type: 'lnum',
-      component: 'colors',
-      label: {
-        de: 'Farbe',
-        it: 'Color',
-        en: 'Color',
-      },
-    },
-    {
-      name: 'designer',
-      type: 'enum',
-      component: 'designer',
-      label: {
-        it: 'Designer',
-        de: 'Designer',
-        en: 'Designer',
-      },
-    },
-  ],
   detailAttributes: [
     {
       name: 'designer',
@@ -166,8 +107,4 @@ const config = {
   ],
   variantSelector: ['color', 'size'],
   variantInProductName: ['size'],
-  ...localConfig,
 };
-// eslint-disable-next-line no-console
-console.log('using config:', config);
-export default config;
